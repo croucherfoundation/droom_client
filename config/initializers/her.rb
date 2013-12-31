6a1ada728da16e59cf86a6e30292079272bf4486
William Ross <will@spanner.org>
2013-12-31 21:19:43 +0000

    new her api interface

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,7 +1,8 @@
 require 'settings'
 require 'paginated_her'
 
-DROOM = Her::API.new base_uri: "#{Settings.droom.protocol}://#{Settings.droom.host}/api" do |c|
+DROOM = Her::API.new
+DROOM.setup url: "#{Settings.droom.protocol}://#{Settings.droom.host}/api" do |c|
   c.use Faraday::Request::UrlEncoded
   c.use PaginatedHer::Middleware::TokenAuth
   c.use PaginatedHer::Middleware::Parser
