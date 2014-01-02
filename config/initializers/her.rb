b45afe28982caab2bf1a0a691787b26ac3f2537d
William Ross <will@spanner.org>
2014-01-02 08:44:13 +0000

    auth port allows localhost service

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,8 +1,11 @@
 require 'settings'
 require 'paginated_her'
 
+Settings.auth[:protocol] ||= 'http'
+Settings.auth[:port] ||= '80'
+
 DROOM = Her::API.new
-DROOM.setup url: "#{Settings.droom.protocol}://#{Settings.droom.host}/api" do |c|
+DROOM.setup url: "#{Settings.auth.protocol}://#{Settings.auth.host}:#{Settings.auth.port}/api" do |c|
   c.use Faraday::Request::UrlEncoded
   c.use PaginatedHer::Middleware::TokenAuth
   c.use PaginatedHer::Middleware::Parser
