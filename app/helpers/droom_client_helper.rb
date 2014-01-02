88c702f87d19c1e0a13574fdc949966b6fbb8a0c
William Ross <will@spanner.org>
2014-01-02 12:47:08 +0000

    more distinct asset links

--- app/helpers/droom_client_helper.rb
+++ app/helpers/droom_client_helper.rb
@@ -5,15 +5,18 @@ module DroomClientHelper
   end
 
   def droom_asset_url(path)
+    Settings.droom[:asset_path] ||= nil
     URI.join(droom_asset_host, Settings.droom.asset_path, path).to_s
   end
 
   def droom_host
-  "#{Settings.droom.protocol}://#{Settings.droom.host}"
+    "#{Settings.droom.protocol}://#{Settings.droom.host}"
   end
 
   def droom_asset_host
-    "http://#{Settings.droom.asset_host}"
+    Settings.droom[:asset_protocol] ||= Settings.droom.protocol
+    Settings.droom[:asset_host] ||= Settings.droom.host
+    "#{Settings.droom.asset_protocol}://#{Settings.droom.asset_host}"
   end
 
 end
