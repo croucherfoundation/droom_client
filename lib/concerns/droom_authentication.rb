6b7e5fdd25e425c910b7b85e05af83ab29201798
William Ross <will@spanner.org>
2014-01-10 05:54:38 +0000

    log redirect details

--- lib/concerns/droom_authentication.rb
+++ lib/concerns/droom_authentication.rb
@@ -90,7 +90,8 @@ protected
   end
 
   def redirect_to_login(exception)
-    unset_auth_cookie(Settings.auth.cookie_domain)
+    Rails.logger.warn "auth redirect: #{exception.inspect}"
+    # unset_auth_cookie(Settings.auth.cookie_domain)
     store_location!
     if is_navigational_format?
       if user_signed_in?
