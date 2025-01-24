5f4edf5faa3b41311088ddf534ec8b590c2514cf
Zin Moe <dev.zinmoe@gmail.com>
2025-01-24 15:22:00 +0630

    "Go to dataroom" from partner admins

--- app/helpers/droom_client_helper.rb
+++ app/helpers/droom_client_helper.rb
@@ -55,25 +55,50 @@ module DroomClientHelper
   end
 
   def determine_dataroom_url(user)
-    whitelist_subdomains = ['projectss', 'projects', 'scholars', 'scholarss', 'search', 'searchs']
-    url = whitelist_subdomains.include?(request.subdomain) ? ENV['DROOM_URL'] : Settings.home_url
-    url_name = whitelist_subdomains.include?(request.subdomain) ? 'Go to data room' : 'Go to public site'
-  
-    return { url: url, text: url_name } if user.admin?
-  
+    whitelist_subdomains = %w[projectss projects scholars scholarss search searchs]
     committees = ['Trustees', 'Audit Committee', 'Investment Committee', 'Nomination Committee', 'Staff']
-    if user.user_groups&.any? { |group| committees.include?(group) }
-      return { url: url, text: url_name }
+
+    # Determine base URL and text based on subdomain
+    base_url = whitelist_subdomains.include?(request.subdomain) ? ENV['DROOM_URL'] : Settings.home_url
+    base_text = whitelist_subdomains.include?(request.subdomain) ? 'Go to data room' : 'Go to public site'
+
+    # Admin users or committee members
+    if user.admin? || user.user_groups&.any? { |group| committees.include?(group) }
+      return { url: base_url, text: base_text }
     end
-  
+
+    # Partner admin users
+    if defined?(Nominator) && defined?(CswAdmin)
+      if (partner_url = partner_admin_url_for_user(user))
+        return { url: partner_url, text: 'Go to data room' }
+      end
+    end
+
+    # Funding applicants or persons linked to the user
     if defined?(Person)
       person = Person.find_by(user_uid: user.uid)
-  
       if person.present? || user.user_groups&.include?('Applicants')
         return { url: "#{Settings.home_url}/funding-application", text: 'Go to data room' }
       end
     end
-  
+
+    # Default case
     { url: nil, text: nil }
-  end  
+  end
+
+  def partner_admin_url_for_user(user)
+    subdomain =
+      if (nominator = Nominator.find_by(user_uid: user.uid))
+        nominator.institution_code
+      elsif (csw_admin = CswAdmin.find_by(user_uid: user.uid))
+        CswPartner.find(csw_admin.csw_partner_id).code
+      end
+
+    return unless subdomain
+
+    subdomain << 's' if Rails.env.staging?
+    domain = Rails.env.development? ? 'croucher.localhost' : 'croucher.org.hk'
+    URI.join("https://#{subdomain}.#{domain}", '/dashboard').to_s
+  end
+
 end
