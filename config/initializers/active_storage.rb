4fc78cba2961e04919c28284fb687c21b0c72709
Thi Ha <thiha.dve@gmail.com>
2025-03-19 13:58:34 +0900

    active storage fix

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,11 +1,11 @@
-Rails.configuration.to_prepare do
-  ActiveStorage::Attachment.class_eval do
-    before_destroy :purge_attachment
+# Rails.configuration.to_prepare do
+#   ActiveStorage::Attachment.class_eval do
+#     before_destroy :purge_attachment
 
-    def purge_attachment
-      Rails.logger.info "Purge attachments..."
-      record = self.record_type.constantize.find(self.record_id).send(self.name)
-      record.purge
-    end
-  end
-end
+#     def purge_attachment
+#       Rails.logger.info "Purge attachments..."
+#       record = self.record_type.constantize.find(self.record_id).send(self.name)
+#       record.purge
+#     end
+#   end
+# end
