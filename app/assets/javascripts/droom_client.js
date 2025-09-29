33fe34b075ad450d92d4ca8226f750f8e80f5972
Zin Moe <dev.zinmoe@gmail.com>
2025-09-29 14:49:28 +0630

    Add password reset failed modal

--- app/assets/javascripts/droom_client.js
+++ app/assets/javascripts/droom_client.js
@@ -1,16 +1,15 @@
-(function() {
-  jQuery(function($) {
-
-    $('.profile-upload-text, #profile-avatar').on('click', function() {
+(function () {
+  jQuery(function ($) {
+    $('.profile-upload-text, #profile-avatar').on('click', function () {
       $('#profile-upload').click();
     });
     // Preview the selected image
-    $('#profile-upload').on('change', function(event) {
+    $('#profile-upload').on('change', function (event) {
       var input = event.target;
 
       if (input.files && input.files[0]) {
         var file = input.files[0];
-        
+
         // Check if the file is an image
         if (!file.type.match('image.*')) {
           alert('Please select an image file only.');
@@ -20,42 +19,48 @@
 
         var reader = new FileReader();
 
-        reader.onload = function(e) {
+        reader.onload = function (e) {
           $('#profile-avatar').attr('src', e.target.result);
           $('.profile-remove-button').css('display', 'block'); // Show remove button
           $('#remove_image').val('false');
-        }
+        };
 
         reader.readAsDataURL(file);
       }
     });
 
-    $('.profile-remove-button').on('click', function(e) {
+    $('.profile-remove-button').on('click', function (e) {
       e.preventDefault();
       $('#remove_image').val('true');
       $('.profile-remove-button').css('display', 'none');
       $('#profile-upload').val('');
-      $('#profile-avatar').attr('src', 'https://cmss.croucher.org.hk/assets/images/croucher-admin-1.png');
+      $('#profile-avatar').attr(
+        'src',
+        'https://cmss.croucher.org.hk/assets/images/croucher-admin-1.png'
+      );
     });
 
     // Password reset form
-    $('.password-reset-btn').on('click', function() {
+    $('.password-reset-btn').on('click', function (e) {
+      e.preventDefault();
+
       var $form = $(this).closest('form');
-      var url = $form.attr('action');
-      var formData = $form.serialize();
 
       $.ajax({
-        url: url,
+        url: $form.attr('action'),
         type: 'POST',
-        data: formData,
-        complete: function(xhr) {
-          if (xhr.status === 302 || xhr.status === 200) {
-            $('#passwordModal').removeClass('modal-open');
-            $("#passwordConfirmModal").addClass('modal-open');
-          }
-        }
-      });
+        data: $form.serialize(),
+      })
+        .done(function (res, status, xhr) {
+          $('#passwordModal').removeClass('modal-open');
+          $('#passwordConfirmModal').addClass('modal-open');
+        })
+        .fail(function (xhr) {
+          errorMsg = xhr.responseJSON.error;
+          $('#passwordModal').removeClass('modal-open');
+          $('#passwordResetFailureModal').find('.alert-message').text(errorMsg);
+          $('#passwordResetFailureModal').addClass('modal-open');
+        });
     });
   });
-
 }).call(this);
--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,20 +2,28 @@ class PasswordsController < ApplicationController
   include DroomAuthentication
   respond_to :json
   skip_before_action :authenticate_user!, raise: false
+  before_action :set_email
 
   def create
+    error_message = I18n.t(:password_reset_not_delivered)
+
+    unless @email_record&.can_receive_email?
+      return render json: { error: error_message }, status: :bad_request
+    end
+
     @user = User.reset_password_request(password_params)
     if @user
-      render json: {message: 'Password reset request sent'}
+      render json: { message: 'Password reset request sent' }
     else
-      render json: {message: 'Password reset request failed'}, status: 302
+      render json: { error: error_message }, status: bad_request
     end
-    
   end
 
-
-
   protected
+
+  def set_email
+    @email_record = Email.where(email: password_params[:email]).first
+  end
 
   def password_params
     if params[:user]
