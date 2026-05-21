ea41516b4a89946ac6a771451b722b10056b2600
kaungmyat <kmyatthu157@gmail.com>
2026-05-21 13:01:59 +0630

    Remove primary and backup email fields from user attributes hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,6 @@ class User
       phone: phone,
       mobile: mobile,
       address: address,
-      primary_email: primary_email,
-      backup_email: backup_email,
       correspondence_address: try(:correspondence_address)
     }
   end
