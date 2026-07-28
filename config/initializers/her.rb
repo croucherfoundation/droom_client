e2fa104a057d4be5eae0a730da2f5886a2522627
Zin Moe <dev.zinmoe@gmail.com>
2026-07-28 10:52:59 +0630

    Handle new API response format with custom parser

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -2,6 +2,7 @@ require 'faraday'
 require 'json'
 require 'her'
 require 'her/middleware/json_api_parser'
+require 'droom_client/middleware/envelope_parser'
 
 api_url = ENV['DROOM_API_URL']
 
@@ -10,8 +11,9 @@ DROOM.setup url: api_url do |c|
   # Request: encode JSON manually
   c.request :json
 
-  # Response: parse JSON API
-  c.use Her::Middleware::JsonApiParser
+  # Response: parse enveloped responses (success/message/<record>),
+  # passing already JSON:API-shaped bodies through untouched.
+  c.use DroomClient::Middleware::EnvelopeParser
 
   # Adapter
   c.adapter Faraday.default_adapter
