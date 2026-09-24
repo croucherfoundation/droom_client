b9a581908818ac6e3279467ddcdbf5ffd52d51e5
yarzardev <yarzarminwai97@gmail.com>
2026-09-24 11:37:43 +0630

    Update user_attributes= method to find user by ID from attributes

--- app/models/concerns/has_droom_user.rb
+++ app/models/concerns/has_droom_user.rb
@@ -92,7 +92,7 @@ module HasDroomUser
   #
   def user_attributes=(attributes)
     if attributes.any?
-      if user = self.user
+      if user = User.find(attributes[:id])
         user.assign_attributes(attributes.with_indifferent_access)
         user.save
       else
